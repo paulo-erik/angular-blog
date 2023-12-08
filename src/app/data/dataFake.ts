35e2a624b4a17ee7b7c723245de40a8401c670c1
Paulo Erik <bonicoc2@gmail.com>
2023-12-08 20:21:54 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    'id':"1",
+    'title': "Meu primeiro projeto com Angular",
+    'description': "Bomba!! Jovem faz o seu primeiro projeto com angular e choca o Brasil",
+    'photoCover':"https://codersera.com/blog/wp-content/uploads/2020/09/images.jpeg-1.jpg",
+  },
+  {
+    'id':"2",
+    'title': "Jovem promissor diz que postará seu primeiro projeto Angular no GitHub",
+    'description': "",
+    'photoCover':"https://miro.medium.com/v2/resize:fit:1400/1*zpUEUWiy51-U4cqhi9CIZA.jpeg",
+  },
+  {
+    'id':"3",
+    'title': "Jovem promete inovação em seu novo projeto",
+    'description': "",
+    'photoCover':"https://www.alura.com.br/artigos/assets/como-comecar-com-angular/como-comecar-com-angular.png",
+  },
+  {
+    'id':"4",
+    'title': "Jovem irá lançar curso Angular gratuitamente em plataforma gigante no mercado",
+    'description': "",
+    'photoCover':"https://www.oditeksolutions.com/wp-content/uploads/2015/10/AngularJS-Framework-and-Features.png",
+  },
+]
